2d01dac65101153e1543aecb9797216e6a9e07ef
Vladimir Grigorov <punktc77@gmail.com>
2025-07-15 17:34:04 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>🐹 What Rikysha Sees</h1>
+      <p>Сайт-визитка о морских свинках, товарах и анимации</p>
     </div>
   );
 }
